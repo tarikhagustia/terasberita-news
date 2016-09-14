22fcb9c4e7d100c2d256483c27904d5d429fec13
Tarikh D. Agustia <agustia.tarikh150@gmail.com>
2016-09-14 16:28:30 +0700

    Ganti footer

--- application/views/back/footer.php
+++ application/views/back/footer.php
@@ -1,6 +1,6 @@
  <footer>
  	<div class="pull-right">
- 		Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+ 		Allblue IT - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
  	</div>
  	<div class="clearfix"></div>
  </footer>
