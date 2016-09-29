819b6ea2388055d0baed63c7e4dd6c339e149f49
Tarikh D. Agustia <agustia.tarikh150@gmail.com>
2016-09-29 17:29:39 +0700

    Tambhan mobile version tapi baru test

---
+++ application/views/FrontOffice/test.php
@@ -0,0 +1,77 @@
+<div class="clearfix">
+</div>
+<div id="not-visible" class="visible-xs">
+  <div id="m_breaking" class="container " style="background-image: url(<?php echo base_url('assets/img/bg.jpg') ?>); ">
+  <!-- <selction id="m_breaking"> -->
+  <!-- </selction> -->
+  </div>
+  <div class="pembatas">
+      <h5>Jerit terakhir dipagi hari</h5>
+      <p>
+        Teras Sukabumi | Kamis, 10 agustus 2016
+      </p>
+  </div>
+  <div class="gap">
+  </div>
+  <!-- <div class="container"> -->
+    <img class="img-ads-header" src="<?php echo base_url('assets/img/iklan/1.png') ?>" />
+  <!-- </div> -->
+  <div class="gap">
+  </div>
+  <div class="m_peristiwa">
+    <div class="m_peristiwa_judul">
+      Teras Peristiwa
+    </div>
+    <div class="gap"></div>
+    <img class="img-responsive" src="<?php echo base_url('assets/img/bg.jpg') ?>" />
+    <div class="gap"></div>
+    <div class="m_peristiwa_judul">
+      <h5>Jerit terakhir dipagi hari</h5>
+      <p class="text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget sodales neque. Quisque sit amet ipsum ac metus euismod tincidunt. Curabitur et neque quis eros euismod cursus in cursus dui. Cras quis turpis dui. Nunc ut iaculis enim. Nullam facilisis dolor nulla, ut hendrerit massa pharetra euismod. Suspendisse consequat dictum lacus.
+      </p>
+    </div>
+  </div>
+  <div class="gap">
+  </div>
+  <div class="m_news_feed">
+    <div class="m_feed_judul">
+      News Feeds
+    </div>
+    <div class="container">
+      <div class="m_fedd_isi">
+        <div class="row">
+          <div class="col-xs-4">
+            <!-- <img class="img-responsive" src="<?php echo base_url('assets/img/bg.jpg') ?>" /> -->
+            <div class="img-feed" style="background-image: url(<?php echo base_url('assets/img/bg.jpg') ?>); "></div>
+          </div>
+          <div class="col-xs-8" style="padding-left: 20px;">
+            <span class="m_fedd_title"><a href="">Pembunuhan menyebabkan kecurigaan pada warga sekitar</a></span>
+            <div class="gap">
+            </div>
+            <p>
+              teras berira | kami, 20 Juni 2016
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="m_fedd_isi">
+        <div class="row">
+          <div class="col-xs-4">
+            <!-- <img class="img-responsive" src="<?php echo base_url('assets/img/bg.jpg') ?>" /> -->
+            <div class="img-feed" style="background-image: url(<?php echo base_url('assets/img/bg.jpg') ?>); "></div>
+          </div>
+          <div class="col-xs-8" style="padding-left: 20px;">
+            <span class="m_fedd_title"><a href="">Pembunuhan menyebabkan kecurigaan pada warga sekitar</a></span>
+            <div class="gap">
+            </div>
+            <p>
+              teras berira | kami, 20 Juni 2016
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="clearfix"></div>
+</div>
